232bf5695b75fd375e64a62ac9c3a89547fabf39
= <=>
2022-04-23 16:33:56 +0900

    viz_merge

--- include/merge_circle.h
+++ include/merge_circle.h
@@ -3,11 +3,12 @@
 #include "geometry_msgs/PoseArray.h"
 #include "geometry_msgs/Pose.h"
 #include "geometry_msgs/PoseStamped.h"
+#include <visualization_msgs/MarkerArray.h>
+#include <visualization_msgs/Marker.h>
 #include "sensor_fusion/Obstacles.h"
 #include "sensor_fusion/CircleObstacle.h"
 #include "sensor_fusion/SegmentObstacle.h"
-#include <visualization_msgs/MarkerArray.h>
-#include <visualization_msgs/Marker.h>
+#include "point.h"
 
 class merge_circle{
     private:
@@ -15,13 +16,27 @@ class merge_circle{
         ros::Subscriber sub_lidar_circle;
         ros::Subscriber sub_camera_circle;
         ros::Publisher markerArrayPub;
+        ros::Publisher camera_markerArrayPub;
+        ros::Publisher merge_markerArrayPub;
+
+        geometry_msgs::PoseArray lidar_circle_array;
+        geometry_msgs::PoseStamped wp;
+        visualization_msgs::MarkerArray D_WPArray;
+        sensor_fusion::Obstacles lidar_circle;
+        sensor_fusion::Obstacles _merge_circle;
+
+        int num = 0; 
 
     public:
         merge_circle();
         ~merge_circle();
 
-        void lidarCallback(sensor_fusion::Obstacles  lidar_circle );
-        visualization_msgs::Marker markergenerator(geometry_msgs::PoseStamped point,double scale,double scale_z, int _id);
-
+        void lidarCallback(sensor_fusion::Obstacles _lidar_circle );
+        void CameraCallback(); // to edit
+        void viz_lidar_circle();
+        void viz_camera_circle();
+        void viz_merge_circle();
+        void merge();
+        visualization_msgs::Marker markergenerator(geometry_msgs::PoseStamped point,double scale,double scale_z, int _id, double color);
 };
 
--- src/merge_circle.cpp
+++ src/merge_circle.cpp
@@ -4,13 +4,16 @@
 
 merge_circle::merge_circle(){
     markerArrayPub = n.advertise<visualization_msgs::MarkerArray>("/viz_lidar_circle",1000);
+    camera_markerArrayPub = n.advertise<visualization_msgs::MarkerArray>("/viz_camera_circle",1000);
+    merge_markerArrayPub = n.advertise<visualization_msgs::MarkerArray>("/viz_merge_circle",1000);
     sub_lidar_circle = n.subscribe("/obstacles", 1000, &merge_circle::lidarCallback, this);
+    
 }
 merge_circle::~merge_circle(){
 
 }
 
-visualization_msgs::Marker merge_circle::markergenerator(geometry_msgs::PoseStamped point,double scale,double scale_z, int _id){
+visualization_msgs::Marker merge_circle::markergenerator(geometry_msgs::PoseStamped point,double scale,double scale_z, int _id, double color){
         visualization_msgs::Marker marker;
         marker.header.stamp = ros::Time::now();
         marker.type = visualization_msgs::Marker::SPHERE;
@@ -24,40 +27,72 @@ visualization_msgs::Marker merge_circle::markergenerator(geometry_msgs::PoseStam
         marker.pose.orientation.z = 0;
         marker.pose.orientation.w = 1.0;
 
-        marker.scale.x = point.pose.position.z*2; //임의의 비행금지구역 반경 (x,y 같게)
-        marker.scale.y = point.pose.position.z*2;
+        marker.scale.x = scale*2; //임의의 비행금지구역 반경 (x,y 같게)
+        marker.scale.y = scale*2;
         marker.scale.z = scale_z; //비행금지구역 높이는 무제한
 
         marker.color.r = 1.0f;
-        marker.color.g = 1;
+        marker.color.g = color;
         marker.color.b = 0.0f;
         marker.color.a = 0.5; //마커 색상 및 투명도
 
-        marker.lifetime = ros::Duration(); //마커 지속시간
+        marker.lifetime = ros::Duration(0.1); //마커 지속시간
         marker.header.frame_id = "/map";
         return marker;
     }
     
 void merge_circle::viz_lidar_circle(){
     D_WPArray.markers.clear();
-    int num = 0;
+    num = 0;
     for (auto& r : lidar_circle.circles){
         wp.pose.position.x = r.center.x;
         wp.pose.position.y = r.center.y;
-        wp.pose.position.z = r.radius;
-        D_WPArray.markers.push_back(markergenerator(wp,0.1,0.1,num));
+        D_WPArray.markers.push_back(markergenerator(wp,r.radius,0.1,num,1.0));
         num++;
     }
     markerArrayPub.publish(D_WPArray);
+}
 
-};
+void merge_circle::viz_merge_circle(){
+    D_WPArray.markers.clear();
+    for (auto& r : _merge_circle.circles){
+        wp.pose.position.x = r.center.x;
+        wp.pose.position.y = r.center.y;
+        D_WPArray.markers.push_back(markergenerator(wp,r.radius,0.1,num,0.5));
+        num++;
+    }
+    merge_markerArrayPub.publish(D_WPArray);
+}
 
-void merge_circle::lidarCallback(sensor_fusion::Obstacles  _lidar_circle ){
+void merge_circle::viz_camera_circle(){
+    D_WPArray.markers.clear();
+    wp.pose.position.x = 0.05;
+    wp.pose.position.y = 0.3;
+    D_WPArray.markers.push_back(markergenerator(wp,0.2,0.1,num,0.0));
+    camera_markerArrayPub.publish(D_WPArray);
+    num++;
+}
+
+void merge_circle::lidarCallback(sensor_fusion::Obstacles _lidar_circle ){
     lidar_circle = _lidar_circle;
 }
 
-void merge_circle::cameraCallback(){
+void merge_circle::CameraCallback(){
 
+}
+
+void merge_circle::merge(){
+    _merge_circle.circles.clear();
+    int j =0;
+    for (auto& r : lidar_circle.circles){
+        double distance = pow(pow(r.center.x-0.05,2)+pow(r.center.y-0.3,2),0.5);
+        if(distance-0.2 < r.radius){
+            _merge_circle.circles.push_back(r);
+            if(distance+0.2 >= r.radius)
+                _merge_circle.circles[j].radius = distance + 0.2;
+            j++;
+        }
+    }
 }
 
 int main(int argc, char** argv){
@@ -65,7 +100,10 @@ int main(int argc, char** argv){
     merge_circle mer;
     ros::Rate r(1000);
     while(ros::ok()){
-        viz_lidar_circle();
+        mer.merge();
+        mer.viz_lidar_circle();
+        mer.viz_camera_circle();
+        mer.viz_merge_circle();
         ros::spinOnce();
         r.sleep();
     }
